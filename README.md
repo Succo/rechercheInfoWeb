cefc38e59516abcbb4546c14fddebb80b091eacb
Fabrice <fabrice.vaillant@centraliens.net>
2016-11-29 16:36:03 +0100

    Adds plot drawing to the output

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A repo with exercices for the "Recherche information web" course. 
+
+Uses gonum/plot to draw the required plots
--- main.go
+++ main.go
@@ -5,14 +5,21 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/gonum/plot"
+	"github.com/gonum/plot/plotter"
+	"github.com/gonum/plot/plotutil"
+	"github.com/gonum/plot/vg"
 )
 
 var cacmFile string
 var commonWordFile string
+var plotFile string
 
 func init() {
 	flag.StringVar(&cacmFile, "cacm", "data/CACM/cacm.all", "Path to cacm file")
 	flag.StringVar(&commonWordFile, "common_word", "data/CACM/common_words", "Path to common_word file")
+	flag.StringVar(&plotFile, "plot", "cacm_plot.png", "Path to output plot file")
 }
 
 func main() {
@@ -39,10 +46,39 @@ func main() {
 	parser.Parse()
 
 	corpusSize := parser.CorpusSize()
-	fmt.Printf("For the whole corpus :\n")
+	fmt.Printf("For the whole corpus (%d) :\n", corpusSize)
 	fmt.Printf("Size of the vocabulary %d\n", parser.IndexSize(corpusSize))
 	fmt.Printf("Number of token %d\n", parser.TokenSize(corpusSize))
-	fmt.Printf("For half the corpus :\n")
+	fmt.Printf("For half the corpus (%d):\n", corpusSize/2)
 	fmt.Printf("Size of the vocabulary %d\n", parser.IndexSize(corpusSize/2))
 	fmt.Printf("Number of token %d\n", parser.TokenSize(corpusSize/2))
+	draw(parser)
+}
+
+func draw(parser *Parser) {
+	corpusSize := parser.CorpusSize()
+
+	plt, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	plt.Title.Text = "Heaps law plot"
+	plt.X.Label.Text = "Text size"
+	plt.Y.Label.Text = "Distinct vocabulary"
+
+	pts := make(plotter.XYs, corpusSize/10+1)
+	for i := 0; i < corpusSize; i += 10 {
+		pts[i/10].X = float64(parser.IndexSize(i))
+		pts[i/10].Y = float64(parser.TokenSize(i))
+	}
+
+	err = plotutil.AddLines(plt, "cacm", pts)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = plt.Save(20*vg.Centimeter, 20*vg.Centimeter, plotFile); err != nil {
+		panic(err)
+	}
 }
