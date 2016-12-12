eea18c7a0c88dffa9a948300b52d3146d64933c3
Fabrice <fabrice.vaillant@centraliens.net>
2016-12-12 15:40:39 +0100

    Adds real search to the TUI

--- tui.go
+++ tui.go
@@ -11,6 +11,7 @@ func dynamicSearch(cacm, cs276 *Search) {
 	line := liner.NewLiner()
 	defer line.Close()
 
+	fmt.Println("Ctrl-C to exit the program")
 	line.SetCtrlCAborts(true)
 	corpus, err := line.Prompt("What corpus do you want to search? 'cacm' or 'cs276'")
 	if err == liner.ErrPromptAborted {
@@ -19,11 +20,24 @@ func dynamicSearch(cacm, cs276 *Search) {
 	} else if err != nil {
 		return
 	}
+	var search *Search
 	if corpus == "cacm" {
-		fmt.Println("Working with cacm")
+		search = cacm
 	} else if corpus == "cs276" {
-		fmt.Println("Working with cs276")
+		search = cs276
 	} else {
 		fmt.Println("Error unsuported option")
+		return
+	}
+	for {
+		if searched, err := line.Prompt("Searched keyword?"); err == nil {
+			results := search.Search(searched)
+			fmt.Printf("Found %d result\n", len(results))
+			for _, result := range results {
+				fmt.Println(result)
+			}
+		} else if err == liner.ErrPromptAborted {
+			return
+		}
 	}
 }
