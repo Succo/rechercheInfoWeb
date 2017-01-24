4721f00630457231458d0bc65341a3bfb8c53544
Fabrice <fabrice.vaillant@centraliens.net>
2017-01-24 12:40:15 +0100

    Removes the character type it's uneeded

--- scanner.go
+++ scanner.go
@@ -2,23 +2,6 @@ package main
 
 import "unicode"
 
-// Character represent a lexical token
-type Character int
-
-const (
-	// Illegal are the ignored charater
-	Illegal Character = iota
-	// EOF is a special character for the End Of File
-	EOF
-	// WS is the character for all whitespaces
-	WS
-
-	// Identifiant is for the marqueurs
-	Identifiant
-	// Token is all the non special words
-	Token
-)
-
 var eof = rune(0)
 
 func tokenMember(ch rune) bool {
