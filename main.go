8619942f8819e8c5c50c422318ac3bebd039e0a0
Fabrice <fabrice.vaillant@centraliens.net>
2017-01-29 22:07:16 +0100

    Makes each index building async

--- main.go
+++ main.go
@@ -26,45 +26,21 @@ func init() {
 }
 
 func main() {
-	log.Println("RIW server started")
+	log.Println("Starting riw server")
 	flag.Parse()
+	c := make(chan *Search)
+	go buildCACM(c)
+	go buildCS276(c)
 	var cacm *Search
 	var cs276 *Search
-	// Get cacm and build related tools
-	if buildIndex {
-		log.Println("Building cacm index from scratch")
-		source, err := os.Open(cacmFile)
-		if err != nil {
-			panic(err)
+	var s *Search
+	for i := 0; i < 2; i++ {
+		s = <-c
+		if s.Corpus == "cacm" {
+			cacm = s
+		} else {
+			cs276 = s
 		}
-		defer source.Close()
-		cacm = ParseCACM(source, commonWordFile)
-		source.Close()
-		cacm.Serialize()
-	} else {
-		log.Println("Loading cacm index from file")
-		cacm = Unserialize("cacm")
-		cacm.Retriever = UnserializeCacmRetriever("cacm")
-	}
-
-	if plotFile != "" {
-		draw(cacm, "cacm")
-	}
-
-	// get cs276 and build related tools
-	if buildIndex {
-		log.Println("Building cs276 index from scratch")
-		now := time.Now()
-		cs276 = ParseCS276(cs276File)
-		log.Printf("cs276 index built in  %s \n", time.Since(now).String())
-		cs276.Serialize()
-	} else {
-		log.Println("Loading cs276 index from file")
-		cs276 = Unserialize("cs276")
-	}
-
-	if plotFile != "" {
-		draw(cs276, "cs276")
 	}
 	serve(cacm, cs276)
 }
@@ -100,4 +76,45 @@ func draw(search *Search, name string) {
 	if err = plt.Save(20*vg.Centimeter, 20*vg.Centimeter, name+plotFile); err != nil {
 		panic(err)
 	}
+}
+
+func buildCACM(c chan *Search) {
+	var cacm *Search
+	if buildIndex {
+		log.Println("Building cacm index from scratch")
+		source, err := os.Open(cacmFile)
+		if err != nil {
+			panic(err)
+		}
+		defer source.Close()
+		cacm = ParseCACM(source, commonWordFile)
+		source.Close()
+		cacm.Serialize()
+	} else {
+		log.Println("Loading cacm index from file")
+		cacm = Unserialize("cacm")
+		cacm.Retriever = UnserializeCacmRetriever("cacm")
+	}
+	if plotFile != "" {
+		draw(cacm, "cacm")
+	}
+	c <- cacm
+}
+
+func buildCS276(c chan *Search) {
+	var cs276 *Search
+	if buildIndex {
+		log.Println("Building cs276 index from scratch")
+		now := time.Now()
+		cs276 = ParseCS276(cs276File)
+		log.Printf("cs276 index built in  %s \n", time.Since(now).String())
+		cs276.Serialize()
+	} else {
+		log.Println("Loading cs276 index from file")
+		cs276 = Unserialize("cs276")
+	}
+	if plotFile != "" {
+		draw(cs276, "cs276")
+	}
+	c <- cs276
 }
