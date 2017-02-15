3029c7e41fb7854a1935e712a7047dbc481e2a45
Fabrice <fabrice.vaillant@centraliens.net>
2017-02-15 19:19:40 +0100

    Adds support for generation of avg graph

--- precision_recall.go
+++ precision_recall.go
@@ -17,6 +17,11 @@ import (
 	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
+const (
+	// The number of point used in the averaged graph
+	approx int = 128
+)
+
 // PreCallCalculator is the struct that caluclate Precision and Recall from queries and answer
 type PreCallCalculator struct {
 	// Queries is a list of string corresponding to queries
@@ -25,8 +30,6 @@ type PreCallCalculator struct {
 	Answer [][]int
 	// Valid is a list of valid graphs (i.e not empty)
 	Valids []int
-	// Average stores the average of all graphs
-	Average [][]float64
 }
 
 // Point is a value in a precision/recall graph
@@ -117,6 +120,10 @@ func (p *PreCallCalculator) Draw(cacm *Search) {
 	// array are initialised with 0, so valids[0] mist be != 0
 	valids[0] = -1
 
+	// Store the average for all valid queries
+	var Average [total][approx]float64
+	var Size [total]int
+
 	for i := range p.Queries {
 		go func(i int) {
 			file := path.Join(dir, strconv.Itoa(i)+".svg")
@@ -164,6 +171,11 @@ func (p *PreCallCalculator) Draw(cacm *Search) {
 				wn := weightName[wf]
 				plt.Legend.Add(wn, line)
 
+				for index := 0; index < approx; index++ {
+					Average[wf][index] += getApproxValue(pts, index)
+				}
+				Size[wf] += 1
+
 			}
 			if !useful {
 				sem <- true
@@ -182,14 +194,55 @@ func (p *PreCallCalculator) Draw(cacm *Search) {
 	for i := 0; i < len(p.Queries); i++ {
 		<-sem
 	}
+
+	var count int
 	newQ := make([]string, 0, len(p.Queries))
 	for i, val := range valids {
 		if i == val {
+			count++
 			p.Valids = append(p.Valids, val)
 			newQ = append(newQ, p.Queries[i])
 		}
 	}
 	p.Queries = newQ
+
+	for j := 0; j < total; j++ {
+		for x := 0; x < approx; x++ {
+			Average[j][x] /= float64(count)
+		}
+	}
+	// Graph for the averages
+	file := path.Join(dir, "avg.svg")
+	plt, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	plt.X.Label.Text = "Recall"
+	plt.Y.Label.Text = "Precision"
+	step := 1 / float64(approx)
+	var pos float64
+	for wf := 0; wf < total; wf++ {
+		pts := make(plotter.XYs, approx)
+		pos = 0
+		for i := 0; i < approx; i++ {
+			pts[i].Y = Average[wf][i]
+			pts[i].X = pos
+			pos += step
+		}
+		line, err := plotter.NewLine(pts)
+		if err != nil {
+			panic(err)
+		}
+		line.Color = colors[wf]
+		line.Width = vg.Points(2)
+		plt.Add(line)
+		wn := weightName[wf]
+		plt.Legend.Add(wn, line)
+	}
+	if err = plt.Save(20*vg.Centimeter, 20*vg.Centimeter, file); err != nil {
+		panic(err)
+	}
 
 	// Generate and save the graph
 	log.Printf("Precision recall graph generated in %s", time.Since(now).String())
@@ -229,4 +282,32 @@ func contains(haystack []int, needle int) bool {
 		}
 	}
 	return false
+}
+
+// getApproxValue returns the approximate value in index/approx
+// for the function having pts as its value
+// It uses liners approximation, pts should have at least 2 points
+func getApproxValue(pts plotter.XYs, index int) float64 {
+	x := float64(index) / float64(approx)
+	pos := -1
+	// Get the last point smaller that x
+	for i, pt := range pts {
+		if pt.X > x {
+			pos = i
+			break
+		}
+	}
+	// get two points for linear interpolation
+	var p1, p2 Point
+	if pos == -1 || pos == len(pts)-1 {
+		// take the two last point as x > pt.x for all pt in pts
+		p1 = pts[len(pts)-2]
+		p2 = pts[len(pts)-1]
+	} else {
+		p1 = pts[pos]
+		p2 = pts[pos+1]
+	}
+	a := (p1.Y - p2.Y) / (p1.X - p2.X)
+	b := p1.Y - a*p1.X
+	return a*x + b
 }
