e7e413a0fb040d1c73822699eba1480f659aaeb2
Fabrice <fabrice.vaillant@centraliens.net>
2017-02-17 08:21:21 +0100

    Fixes test for concurrent trie

--- trie_test.go
+++ trie_test.go
@@ -59,19 +59,21 @@ var fakeWords = [][]byte{
 
 func TestTrie(t *testing.T) {
 	testDeltas := make([]int, len(testWords))
-	testTfIds := make([]float64, len(testWords))
+	testTfIds := make([]weights, len(testWords))
 	trie := NewTrie()
 	for i, w := range testWords {
 		testDeltas[i] = int(i)
-		testTfIds[i] = float64(i)
-		trie.set(w, []uint{uint(i)}, []float64{float64(i)})
+		testTfIds[i][0] = float64(i)
+		var wf weights
+		wf[0] = float64(i)
+		trie.add(w, uint(i), wf)
 	}
 	for i, w := range testWords {
 		resp := trie.get(w)
 		if len(resp) != 1 {
 			t.Fatal("Incorrect result size for inserted word")
 		} else if resp[0].Id != testDeltas[i] ||
-			resp[0].TfIdf != testTfIds[i] {
+			resp[0].Weights != testTfIds[i] {
 			t.Fatal("Incorrect result for inserted word")
 		}
 	}
