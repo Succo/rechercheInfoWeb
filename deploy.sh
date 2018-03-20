484dd00c18f094a6c4e137bf1103c8705dbbb24a
Fabrice <fabrice.vaillant@centraliens.net>
2018-03-20 21:07:33 +0100

    Removes the vendor directory from binary deploys

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 go install
 if [ "$1" == "lite" ]
 	then
-		rsync -avz --progress . fabrice@succo.fr:riw/ --exclude ".git" --exclude "data" --exclude "indexes" 
+		rsync -avz --progress . fabrice@succo.fr:riw/ --exclude ".git" --exclude "data" --exclude "indexes" --exclude "vendor"
 	else 
-		rsync -avz --progress . fabrice@succo.fr:riw/ --exclude ".git" --exclude "data"
+		rsync -avz --progress . fabrice@succo.fr:riw/ --exclude ".git" --exclude "data" --exclude "vendor"
 fi
 # A bit wonky
 rsync -avz --progress $GOPATH/bin/rechercheInfoWeb fabrice@succo.fr:
